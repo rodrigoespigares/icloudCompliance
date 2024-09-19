507d05beffc997ac1a5e07121747c8b5d6bd3f09
rodrigoespigares <rodrigoespigaresfernandez@gmail.com>
2024-09-19 18:42:55 +0200

    [add] interface Document

--- codigo/resources/js/types/index.d.ts
+++ codigo/resources/js/types/index.d.ts
@@ -6,6 +6,17 @@ export interface User {
     permission: number;
 }
 
+export interface Document {
+    id: number;
+    name: string;
+    description: string;
+    priority: number;
+    date_approved?: string;
+    date_submitted: string;
+    url: string;
+    status: number;
+}
+
 export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
     auth: {
         user: User;
