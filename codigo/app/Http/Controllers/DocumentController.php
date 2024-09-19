80decde63ddfde86b1e1d4c286760328dc547d26
rodrigoespigares <rodrigoespigaresfernandez@gmail.com>
2024-09-19 18:42:36 +0200

    [create] Create document model, controller and migration

---
+++ codigo/app/Http/Controllers/DocumentController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Document;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class DocumentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $documents = Document::where('status', 1)->get();
+
+        return Inertia::render('Dashboard', [
+            'documents' => $documents
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validate = $request->validate([
+            'name' => 'required|string|max:50',
+            'description' => 'required|string|max:255',
+            'priority' => 'required|integer|min:1|max:3',
+        ]);
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Document $document)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Document $document)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Document $document)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Document $document)
+    {
+        //
+    }
+}
