2542b27f3683b949981473d7585eaabd4bd18305
rodrigoespigares <rodrigoespigaresfernandez@gmail.com>
2024-09-19 18:43:16 +0200

    [edit] Dashboar to see document but in simple list

--- codigo/resources/js/Pages/Dashboard.tsx
+++ codigo/resources/js/Pages/Dashboard.tsx
@@ -1,17 +1,31 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
+import { Document } from '@/types';
 
-export default function Dashboard() {
+
+export default function Dashboard({ documents = [] }: { documents?: Document[] }) {
     return (
         <AuthenticatedLayout
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Home</h2>}
         >
             <Head title="Dashboard" />
 
             <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
+                <div className="max-w-7xl mx-auto sm:px-3 lg:px-8">
+
+                    <div className="mt-1 p-3 rounded-md bg-white">
+                        <h3 className="font-semibold text-lg">Documentos:</h3>
+                        <ul>
+                            {documents && documents.length > 0 ? (
+                                documents.map((document) => (
+                                    <li key={document.id}>
+                                        {document.name} - {new Date(document.date_submitted).toLocaleDateString()}
+                                    </li>
+                                ))
+                            ) : (
+                                <li>No hay documentos disponibles.</li>
+                            )}
+                        </ul>
                     </div>
                 </div>
             </div>
