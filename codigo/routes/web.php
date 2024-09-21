85540bee260e2050b0455ce7040a74617a35aaaa
rodrigoespigares <rodrigoespigaresfernandez@gmail.com>
2024-09-21 13:55:19 +0200

    [add] Routes for uploading documents

--- codigo/routes/web.php
+++ codigo/routes/web.php
@@ -16,6 +16,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/documents', [DocumentController::class, 'documents']);
+    Route::post('/documents', [DocumentController::class, 'store'])->name('documents.store');
+    Route::patch('/documents/{document}', [DocumentController::class, 'update'])->name('documents.update');
 });
 
 require __DIR__.'/auth.php';
