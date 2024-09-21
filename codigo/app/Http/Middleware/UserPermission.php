cbcfaff4df7256a27e452f0f9fb4b127099b36e1
rodrigoespigares <rodrigoespigaresfernandez@gmail.com>
2024-09-22 01:08:48 +0200

    [add] Middleware for secure routes only admin

---
+++ codigo/app/Http/Middleware/UserPermission.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserPermission
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+
+        if (Auth::user()->permissions == 0) {
+            abort(404, 'No tienes permisos para acceder a esta página');
+        }
+        return $next($request);
+    }
+}
