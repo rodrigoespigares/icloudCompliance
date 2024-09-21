f506a79fdc2fa06a0effa5b071265f8c66c366fc
rodrigoespigares <rodrigoespigaresfernandez@gmail.com>
2024-09-22 01:12:44 +0200

    [add] Using middleware to protect routes

--- codigo/routes/web.php
+++ codigo/routes/web.php
@@ -4,7 +4,9 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\DocumentController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\UserPermission;
 use App\Models\Document;
+use App\Models\User;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -25,7 +27,12 @@ Route::get('/home', function (){
         'permissions' => $permissions
     ]);
 })->middleware(['auth', 'verified'])->name('dashboard');
-Route::get('/graficos', [DocumentController::class, "stats"])->middleware(['auth', 'verified'])->name('graficos');
+
+Route::middleware(UserPermission::class)->group(function () {
+    Route::get('/graficos', [DocumentController::class, "stats"])->middleware(['auth', 'verified'])->name('graficos');
+});
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -33,43 +40,44 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('/documents', [DocumentController::class, 'documents'])->name('documents.index');
-    Route::post('/documents', [DocumentController::class, 'store'])->name('documents.store');
-    Route::post('/documents/{id}', [DocumentController::class, 'update'])->name('documents.update');
-    Route::delete('/documents/{id}', [DocumentController::class, 'destroy'])->name('documents.destroy');
-    Route::patch('/documents/{id}', [DocumentController::class, 'approve'])->name('documents.approve');
+
+    Route::middleware(UserPermission::class)->group(function () {
 
 
+        Route::post('/documents', [DocumentController::class, 'store'])->name('documents.store');
+        Route::post('/documents/{id}', [DocumentController::class, 'update'])->name('documents.update');
+        Route::delete('/documents/{id}', [DocumentController::class, 'destroy'])->name('documents.destroy');
+        Route::patch('/documents/{id}', [DocumentController::class, 'approve'])->name('documents.approve');
+        Route::get('/users', [UserController::class, 'index']);
 
-    Route::get('/users', [UserController::class, 'index']);
+        Route::get('/view-pdf/{id}', function ($id) {
 
-
-    Route::get('/view-pdf/{id}', function ($id) {
-
-        $document = Document::findOrFail($id);
-
-        if(Auth::user()->permissions == 0){
-            if ($document->user_id != Auth::user()->id) {
+            $document = Document::findOrFail($id);
+    
+            if(Auth::user()->permissions == 0){
+                if ($document->user_id != Auth::user()->id) {
+                    abort(404, 'Archivo no encontrado');
+                }
+            }
+    
+            $filePath =  $document->url;
+            
+            $file = explode('/', $filePath);
+    
+            var_dump($file[count($file) - 1]);
+        
+            if (Storage::disk('private')->exists($filePath)) {
+                $fileContent = Storage::disk('private')->get($filePath);
+                $mimeType = File::mimeType(storage_path('app/private/' . $filePath));
+        
+                return response($fileContent, 200)
+                    ->header('Content-Type', $mimeType)
+                    ->header('Content-Disposition', 'inline; filename="' . $file[count($file) - 1] . '"');
+            } else {
                 abort(404, 'Archivo no encontrado');
             }
-        }
-
-        $filePath =  $document->url;
-        
-        $file = explode('/', $filePath);
-
-        var_dump($file[count($file) - 1]);
-    
-        if (Storage::disk('private')->exists($filePath)) {
-            $fileContent = Storage::disk('private')->get($filePath);
-            $mimeType = File::mimeType(storage_path('app/private/' . $filePath));
-    
-            return response($fileContent, 200)
-                ->header('Content-Type', $mimeType)
-                ->header('Content-Disposition', 'inline; filename="' . $file[count($file) - 1] . '"');
-        } else {
-            abort(404, 'Archivo no encontrado');
-        }
-    })->middleware('auth')->name('view-pdf');
+        })->middleware('auth')->name('view-pdf');
+    });
 });
 
 require __DIR__.'/auth.php';
