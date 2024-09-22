b84c5ea5a40646d0e42d86650b87c041c3fe8ca9
rodrigoespigares <rodrigoespigaresfernandez@gmail.com>
2024-09-22 15:36:23 +0200

    [remove] Api documents

--- codigo/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\DocumentController;
-use Illuminate\Support\Facades\Route;
-
-
-Route::get('documents', [DocumentController::class, 'indexJson'])->name('documents.Json'); 
-Route::get('documents/{id}', [DocumentController::class, 'showJson'])->name('documents.show');
